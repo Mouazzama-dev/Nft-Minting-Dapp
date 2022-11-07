7c300909a07c5401d0f844e2d1fae3f03bb19ab9
Mouazzama <mouazzamabinteumer.ep19102044@gmail.com>
2022-11-07 05:20:08 -0800

    file

--- next js/pages/api/[tokenId].js
+++ next js/pages/api/[tokenId].js
@@ -4,8 +4,8 @@ export default function handler(req, res) {
 
   const name =  `Casual Nft Mint # ${tokenId}`;
   const description = `Casual Nft is collection for web3 developer`
-  const image = `https://github.com/Mouazzama-dev/Nft-Minting-dapp/blob/main/next%20js/public/cryptodevs/${Number(tokenId-1)}.svg`
-
+  // const image = `https://github.com/Mouazzama-dev/Nft-Minting-dapp/blob/main/next%20js/public/cryptodevs/${Number(tokenId-1)}.svg`
+  const image = `https://gateway.pinata.cloud/ipfs/QmaWAQsgPjDv65h6ahU2i4jtwbVGNtdAK71WbG8XzZ4F8p/`
   return res.json({
     name: name,
     description: description,
